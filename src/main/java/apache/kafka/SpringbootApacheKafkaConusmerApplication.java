48061adddd7e0c684f396bc9b644fb75d974f06b
hong-il <hongil.kim.dev@gmail.com>
2022-07-13 13:58:12 +0900

    Initial commit

---
+++ src/main/java/apache/kafka/SpringbootApacheKafkaConusmerApplication.java
@@ -0,0 +1,13 @@
+package apache.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafkaConusmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApacheKafkaConusmerApplication.class, args);
+    }
+
+}
